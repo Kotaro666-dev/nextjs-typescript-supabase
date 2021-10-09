f0a9aeaeb5c856e279612176159eaa9be02d758e
Kotaro Kamashima <kotaro0726@gmail.com>
2021-10-10 08:40:41 +0900

    Update: Add session

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,19 @@
+import React, { useEffect } from "react";
+import { supabase } from "../components/helper/SupabaseClient";
+import { authActions } from "../store/auth-slice";
+import { useAuthDispatch } from "../store/store";
+
 const HomePage = () => {
+  const authDispatch = useAuthDispatch();
+
+  useEffect(() => {
+    const session = supabase.auth.session();
+    console.log(session);
+    if (session) {
+      authDispatch(authActions.signIn(session.user?.id));
+    }
+  }, [authDispatch]);
+
   return <h1>Hello world!</h1>;
 };
 
