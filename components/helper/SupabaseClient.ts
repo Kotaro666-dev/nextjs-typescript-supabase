609765b10f1aa982003a37a168c2267251d08305
Kotaro Kamashima <kotaro0726@gmail.com>
2021-10-10 08:21:52 +0900

    Update: Add supabase client

--- components/helper/SupabaseClient.ts
+++ components/helper/SupabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseApiUrl = process.env.API_URL as string;
-const supabaseApiKey = process.env.API_KEY as string;
+const supabaseApiUrl = process.env.SUPABASE_URL as string;
+const supabaseApiKey = process.env.SUPABASE_KEY as string;
 
 export const supabase = createClient(supabaseApiUrl, supabaseApiKey);
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_KEY: process.env.SUPABASE_KEY,
+  },
+};
