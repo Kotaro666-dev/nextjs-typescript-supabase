8ac51aeb2bb8f0844f333d3b45461799906c4629
Kotaro Kamashima <kotaro0726@gmail.com>
2021-10-09 21:29:55 +0900

    Update: Add isLoggedin login to navigation bar

--- components/Layout/MainNavigation.tsx
+++ components/Layout/MainNavigation.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
 
 import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
   const router = useRouter();
+  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
 
   const onLogoClickHandler = () => {
     router.push("/");
@@ -18,15 +21,9 @@ const MainNavigation = () => {
 
       <nav>
         <ul>
-          <li>
-            <Link href="/signin">SignIn</Link>
-          </li>
-          <li>
-            <Link href="/signup">SignUp</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          <li>{!isLoggedIn && <Link href="/signin">SignIn</Link>}</li>
+          <li>{!isLoggedIn && <Link href="/signup">SignUp</Link>}</li>
+          <li>{isLoggedIn && <button>Logout</button>}</li>
         </ul>
       </nav>
     </header>
